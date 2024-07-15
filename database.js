69e4966a6021b5e41ddf67d9cf523962802f106f
Simcard01668 <howingyam0350@gmail.com>
2024-07-15 16:04:18 +0800

    configure MySQL database connection

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 const mysql2 = require('mysql2')
 
-const pool = mysql2.createPool({
-    host:'localhost',
-    user:'root',
-    password:'137958426',
-    database:'Chat'
-})
+// const pool = mysql2.createPool({
+//     host:'localhost',
+//     user:'root',
+//     password:'137958426',
+//     database:'Chat'
+// })
+const pool = mysql.createPool(process.env.b8c0a1f6e66648:d605ad76@us-cluster-east-01.k8s.cleardb.net/heroku_9ccb0de730c94e1?reconnect=true);
 
 module.exports = pool.promise()
