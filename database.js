aac97b0003b71009e994622e5c048e2bd71bc3fa
Simcard01668 <howingyam0350@gmail.com>
2024-07-15 16:15:13 +0800

    redeploy

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const mysql2 = require('mysql2')
+const mysql = require('mysql2')
 
 // const pool = mysql2.createPool({
 //     host:'localhost',
@@ -6,6 +6,7 @@ const mysql2 = require('mysql2')
 //     password:'137958426',
 //     database:'Chat'
 // })
-const pool = mysql.createPool(process.env.b8c0a1f6e66648:d605ad76@us-cluster-east-01.k8s.cleardb.net/heroku_9ccb0de730c94e1?reconnect=true);
 
-module.exports = pool.promise()
+const pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
+
+module.exports = pool.promise();
