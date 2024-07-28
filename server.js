f70e6f2cef69001e54e6faed25bd3e814927e5a3
IsaacYam <howingyam0350@gmail.com>
2024-07-28 09:17:29 +0800

    minor adjustment

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ io.on('connection', (socket) => {
 
 // -------------------------------------------------------------
 //start server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
